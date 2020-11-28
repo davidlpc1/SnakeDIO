6000877dcb866c28ac194d7c10aa99d8fc4c0ad9
davidlpc1 <davipadro76@gmail.com>
2020-11-28 16:36:56 -0300

    Everthing ready!

--- script.js
+++ script.js
@@ -1,81 +1,84 @@
-const canvas = document.getElementById('snake');
-const context = canvas.getContext('2d');
-const square = 32;
-const snake = [
+const canvas = document.getElementById("snake"); 
+const context = canvas.getContext("2d"); 
+const box = 32;
+let snake = [
     {
-        x:8 * square,
-        y:8 * square,
+        x: 8 * box,
+        y: 8 * box
     }
-];
-let direction = 'right';
-let food = {
-    x: Math.floor(Math.random() * 15 + 1) * square,
-    y: Math.floor(Math.random() * 15 + 1) * square,
+]; 
+
+let direction = "right";
+const food ={
+    x: Math.floor(Math.random() * 15 + 1) * box,
+    y: Math.floor(Math.random() * 15 + 1) * box
 }
 
-function createBackground(){
-    context.fillStyle = '#ccc';
-    context.fillRect(0 ,0 ,16* square,16 * square)
+function criarBG(){
+    context.fillStyle = "#ccc";
+    context.fillRect(0, 0, 16*box, 16*box); 
 }
 
-function createSnake(){
-    for (let i = 0; i < snake.length;i++){
-        context.fillStyle = '#000';
-        context.fillRect(snake[i].x,snake[i].y,square,square)
+function criarCobrinha (){
+    for(i = 0; i < snake.length; i++){
+        context.fillStyle = "#000";
+        context.fillRect(snake[i].x, snake[i].y, box, box);
     }
 }
 
-function drawFood(){
-    context.fillStyle = '#f00';
-    context.fillRect(food.x, food.y, square ,square)
+function drawFood (){
+    context.fillStyle = "red";
+    context.fillRect(food.x, food.y, box, box);
 }
 
-document.addEventListener('keydown',update);
+document.addEventListener('keydown', update);
 
 function update(event){
-    let controls = {
-        ArrowDown(){
-            direction = 'down'
-        },
-        ArrowUp(){
-            direction = 'up'
-        },
-        ArrowRight(){
-            direction = 'right';
-        },
-        ArrowLeft(){
-            direction = 'left'
-        }
-    }
-    if(!controls[event.key]) return;
-    controls[event.key]();
+    if(event.keyCode == 37 ) direction = 'left';
+    if(event.keyCode == 38) direction = 'up';
+    if(event.keyCode == 39 ) direction = 'right';
+    if(event.keyCode == 40) direction = 'down';
 }
 
-function initGame(){
-    createBackground();
-    createSnake();
+function iniciarJogo(){    
+
+    if(snake[0].x > 15*box && direction == "right") snake[0].x = 0;
+    if(snake[0].x < 0 && direction == 'left') snake[0].x = 16 * box;
+    if(snake[0].y > 15*box && direction == "down") snake[0].y = 0;
+    if(snake[0].y < 0 && direction == 'up') snake[0].y = 16 * box;
+    
+    for(i = 1; i < snake.length; i++){
+        if(snake[0].x == snake[i].x && snake[0].y == snake[i].y){
+            clearInterval(jogo);
+            alert('Game Over :(');
+        }
+    }
+
+    criarBG();
+    criarCobrinha();
     drawFood();
 
     let snakeX = snake[0].x;
     let snakeY = snake[0].y;
 
-    if(snakeX >= 512) snakeX = 1;
-    if(snakeY >= 512) snakeY = 1;
-    if(snakeX <= 0) snakeX = 512;
-    if(snakeY <= 0) snakeY = 512;
+    if(direction == "right") snakeX += box;
+    if(direction == "left") snakeX -= box;
+    if (direction == "up") snakeY -= box;
+    if(direction == "down") snakeY += box;
 
-    if(direction == 'right') snakeX += square;
-    if(direction == 'left') snakeX -= square;
-    if(direction == 'up') snakeY -= square;
-    if(direction == 'down') snakeY += square;
-
-    if(snakeX != food.x || snakeY != food.y) snake.pop();
-    else{
-        food.x = Math.floor(Math.random() * 15 + 1) * square;
-        food.y = Math.floor(Math.random() * 15 + 1) * square;
+    if(snakeX != food.x || snakeY != food.y){
+        snake.pop();
+    }else{
+        food.x = Math.floor(Math.random() * 15 +1) * box;
+        food.y = Math.floor(Math.random() * 15 +1) * box;
+    }
+    
+    let newHead ={
+        x: snakeX,
+        y: snakeY
     }
 
-    snake.unshift({ x: snakeX, y: snakeY });
+    snake.unshift(newHead); 
 }
 
-const game = setInterval(initGame,100);
+const jogo = setInterval(iniciarJogo, 100);
